4ca5624e68a663fb8554557628a9551526fa8924
Chalex <177287665+chalexmarmol@users.noreply.github.com>
2025-06-24 16:29:41 +0800

    done 3.8

---
+++ part3/phonebook/index.js
@@ -0,0 +1,120 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json());
+
+morgan.token('req-body', (req, res) => {
+  if (req.method === 'POST' && req.body) {
+    return JSON.stringify(req.body);
+  }
+
+  return '-';
+});
+
+app.use(
+  morgan(
+    `:method :url :status :res[content-length] - :response-time ms || Body: :req-body`
+  )
+);
+let persons = [
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+];
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    res.json(person);
+  } else {
+    res.statusMessage = 'Person not found';
+    res.status(404).end();
+  }
+});
+
+app.get('/info', (req, res) => {
+  const date = new Date();
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>`
+  );
+});
+
+const generateRandomId = () => {
+  const id = Math.floor(Math.random() * 100000000);
+
+  if (persons.find((person) => person.id === id)) {
+    return generateRandomId();
+  }
+
+  return String(id);
+};
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name or number missing',
+    });
+  }
+
+  if (persons.find((person) => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique',
+    });
+  }
+
+  const person = {
+    id: generateRandomId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = [...persons, person];
+
+  res.json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!persons.find((person) => person.id === id)) {
+    res.statusMessage = 'Person not found';
+    return res.status(404).end();
+  }
+
+  persons = persons.filter((person) => person.id !== id);
+  res.statusMessage = 'Person deleted';
+  res.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.clear();
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
